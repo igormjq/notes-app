e69536c15250a1ac87ab22b79fba82f3119328be
igormjq <igormjq@gmail.com>
2017-10-27 22:31:15 -0200

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+console.log(`starting app.js`);
+
+const notes = require('./notes');
+const yargs = require('yargs');
+
+const argv = yargs.argv;
+const command = argv._[0];
+
+switch(command) {
+    case 'add':
+        let note = notes.addNote(argv.title, argv.body);
+        
+        if(note)
+            return console.log(`Note created: ${note.title}, ${note.body}`);
+        
+        console.log('Title already in use. Duplicate titles not allowed!');
+
+        break;
+    case 'remove':
+        let noteRemoved = notes.removeNote(argv.title);
+        
+        let message = noteRemoved ? `Note "${argv.title}" removed` : `Note titled "${argv.title}" not found.`;
+        
+        console.log(message);
+
+        break;
+    case 'read':
+        let targetNote = notes.getNote(argv.title);
+        
+        return targetNote ? console.log(targetNote) : console.log(`Note titled "${argv.title}" not found.`);
+
+        break;
+    case 'list':
+        let list = notes.getAll();
+        console.log('Notes:');
+        console.log(list);
+        
+        break;
+    default:
+        console.log('Command not recognized');
+}
---
+++ notes.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+
+// Global functions
+
+let saveNotes = (data) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(data));
+};
+
+let fetchNotes = () => {
+    try {
+        return JSON.parse(fs.readFileSync('notes-data.json'));
+    } catch(e) {
+        return [];
+    }
+};
+
+// CRLD Methods
+
+let addNote = (title, body) => {
+
+    let notes = fetchNotes();
+
+    let note = { title, body };
+
+    let uniqueTitle = !(notes.some(n => n.title === title));
+
+    if(uniqueTitle) {
+        notes.push(note);
+        saveNotes(notes);
+        
+        return note;
+    };
+      
+};
+
+let removeNote = (title) => {
+    
+    let originalNotes = fetchNotes();
+
+    let filteredNotes = originalNotes
+        .filter(note => note.title !== title);
+    
+    saveNotes(filteredNotes);
+
+    return originalNotes.length > filteredNotes.length;
+};
+
+let getNote = (title) => fetchNotes().find(current => current.title === title);
+
+let getAll = () => fetchNotes();
+
+module.exports = {
+    addNote,
+    removeNote,
+    getAll,
+    getNote
+};
